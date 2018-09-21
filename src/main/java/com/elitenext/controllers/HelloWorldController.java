2bb5d1f8d273c3419ddeee1c486c8c59f0f8bbbc
Abdul M Mohammed <muqtadir81@yahoo.com>
2018-09-20 23:31:15 -0400

    Helloworld controller and htl added

---
+++ src/main/java/com/elitenext/controllers/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.elitenext.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+	@RequestMapping("//")
+	public String hello() {
+		return "index";
+	}
+
+}
